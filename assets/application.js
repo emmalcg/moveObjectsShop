39b459a53c65f2378d7b606ea8747aa31a9d8921
emma <emmalcgreen@gmail.com>
2021-06-10 19:34:39 -0400

    quantity button working inside cart

--- assets/application.js
+++ assets/application.js
@@ -1,77 +1,93 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener ('DOMContentLoaded', () => {
 
     const moveApp = {}
 
+    //make number function from a string
     moveApp.makeNumber = (string) => {
         return parseInt(string.replace(/\D/g,''))
     }
-    moveApp.productPriceText = document.querySelector('.product-price').innerHTML;
-    moveApp.productPrice = moveApp.makeNumber(moveApp.productPriceText)
 
+    //make number to display in "Add to Cart" button on product page 
+    moveApp.productPriceTextEl = document.querySelector('.product-price')
+    console.log(moveApp.productPriceTextEl)
+    if (moveApp.productPriceTextEl !== null) {
+        moveApp.productPriceText = moveApp.productPriceTextEl.innerHTML
+        moveApp.productPrice = moveApp.makeNumber(moveApp.productPriceText)
+    } 
     moveApp.buttonPriceEl = document.querySelector('.add-cart-button-price')
-    console.log(moveApp.buttonPriceEl)
+
+    //quantity fields 
 
 
-    //quantity button functionality
-    moveApp.minusButtonEl = document.querySelector('button.minus');
-    moveApp.plusButtonEl = document.querySelector('button.plus')
-    moveApp.inputQuantityEl = document.querySelector('.js-quantity-field')
 
-    moveApp.quantityMax = quantity.getAttribute('max') ? parseInt(quantity.getAttribute('max')) : null; 
+    moveApp.quantityPicker = () => {
+        const minusButtonEls = document.querySelectorAll('button.minus');
+        const plusButtonEls = document.querySelectorAll('button.plus')
+        
+        //create synthetic event to run every time button is click
+        const changeQuantity = new Event('input');
 
-    //create synthetic event to run every time button is click
-    moveApp.changeQuantity = new Event('input');
+        function increaseCount (e) {
+            const inputQuantityEl = this.parentElement.querySelector('.js-quantity-field')
 
-    moveApp.increaseCount = () => {
-        let value = parseInt(moveApp.inputQuantityEl.value)
-        if (moveApp.quantityMax) {
-            if (value < moveApp.quantityMax) {
-                value++
-                moveApp.inputQuantityEl.value = value
-                moveApp.inputQuantityEl.dispatchEvent(moveApp.changeQuantity);
+            const quantityMax = inputQuantityEl.getAttribute('max') ? parseInt(inputQuantityEl.getAttribute('max')) : null; 
+            // console.log(e.target)
+            let value = parseInt(inputQuantityEl.value)
+            if (quantityMax) {
+                if (value < quantityMax) {
+                    value++
+                    inputQuantityEl.value = value
+                    inputQuantityEl.dispatchEvent(changeQuantity);
+                }
+            }
+
+            const quantityTextEl = this.parentElement.querySelector('.quantity-text')
+            quantityTextEl.innerHTML = inputQuantityEl.value
+
+            //add total price text to "Add to Cart" button
+            if(moveApp.productPriceTextEl !== null) {
+                const quantityPrice = value * moveApp.productPrice
+                moveApp.buttonPriceEl.innerHTML = `$${quantityPrice}.00`
             }
         }
-        const quantityPrice = value * moveApp.productPrice
-        moveApp.buttonPriceEl.innerHTML = `$${quantityPrice}.00`
+
+        function decreaseCount () {
+            const inputQuantityEl = this.parentElement.querySelector('.js-quantity-field')
+    
+            const quantityMax = inputQuantityEl.getAttribute('max') ? parseInt(inputQuantityEl.getAttribute('max')) : null; 
+
+            let value = parseInt(inputQuantityEl.value)
+
+            if (value > 1) {
+                value--
+                inputQuantityEl.value = value
+
+                inputQuantityEl.dispatchEvent(changeQuantity);
+            }
+
+            const quantityTextEl = this.parentElement.querySelector('.quantity-text')
+            quantityTextEl.innerHTML = inputQuantityEl.value
+
+            //add total price text to "Add to Cart" button
+            if(moveApp.productPriceTextEl !== null) {
+                const quantityPrice = value * moveApp.productPrice
+                moveApp.buttonPriceEl.innerHTML = `$${quantityPrice}.00`
+            }
+        } 
+
+        minusButtonEls.forEach(button => {
+            button.addEventListener('click', decreaseCount)
+        })
+
+        plusButtonEls.forEach(button => {
+            button.addEventListener('click', increaseCount)
+        })
     }
 
-    moveApp.decreaseCount = () => {
-        let value = parseInt(moveApp.inputQuantityEl.value)
-        if (value > 1) {
-            value--
-            moveApp.inputQuantityEl.value = value
+    moveApp.init = () => {
+        moveApp.quantityPicker()
+    }
 
-            moveApp.inputQuantityEl.dispatchEvent(moveApp.changeQuantity);
-        }
+    moveApp.init()
 
-        const quantityPrice = value * moveApp.productPrice
-        moveApp.buttonPriceEl.innerHTML = `$${quantityPrice}.00`
-    } 
-
-    moveApp.minusButtonEl.addEventListener('click', moveApp.decreaseCount)
-    moveApp.plusButtonEl.addEventListener('click', moveApp.increaseCount)
-
-
-    //listen for change on input and add to text-span 
-    moveApp.inputQuantityEl.addEventListener('input', e => {
-        const quantityTextEl = document.querySelector('.quantity-text')
-        quantityTextEl.innerHTML = moveApp.inputQuantityEl.value
-        
-        //enable/disable minus button based on value
-        moveApp.shouldDisableMinus = parseInt(moveApp.inputQuantityEl.value) === 1;
-        if (moveApp.shouldDisableMinus) {
-            moveApp.minusButtonEl.disabled = true
-        } else if (moveApp.minusButtonEl.disabled = true) {
-            moveApp.minusButtonEl.disabled = false
-        }
-
-        //enable/disable plus button based on value
-        moveApp.shouldDisablePlus = parseInt(moveApp.inputQuantityEl.value) === moveApp.quantityMax
-
-        if (moveApp.shouldDisablePlus) {
-            moveApp.plusButtonEl.disabled = true
-        } else if (moveApp.plusButtonEl.disabled = true) {
-            moveApp.plusButtonEl.disabled = false
-        }
-    })
 })
